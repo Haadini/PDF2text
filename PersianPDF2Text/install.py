04a2647e094a62937078861c03739becd4c3fbb0
Hadi Heydari <hadiheydarimw1375@gmail.com>
2023-06-26 15:25:12 +0200

    Update install.py

--- PersianPDF2Text/install.py
+++ PersianPDF2Text/install.py
@@ -2,13 +2,14 @@ import subprocess
 
 def install_packages():
 
-    subprocess.check_call(["pip", "install", "poppler-utils", "libpoppler-cpp-dev", "tesseract-ocr", "libtesseract-dev", "tesseract-ocr-fas"])
+    subprocess.check_call(["pip", "install", "tqdm", "pdf2image", "pytesseract", "Pillow", "python-docx", "PyPDF2"])
 
 def install_system_packages():
 
-    subprocess.check_call(["apt-get", "install", "-y", "tqdm", "pdf2image", "pytesseract", "Pillow", "python-docx", "PyPDF2"])
+    subprocess.check_call(["apt-get", "install", "-y", "poppler-utils", "libpoppler-cpp-dev", "tesseract-ocr", "libtesseract-dev", "tesseract-ocr-fas"])
 
 install_packages()
 
 install_system_packages()
+
 
